3d6cb52d304df6c12bfd01a00a091997300b0cd5
carpentryplus25 <williamt1984@yahoo.com>
2017-10-31 07:14:52 -0400

    Initial Commit

---
+++ AMDPatcher/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  AMDPatcher
+//
+//  Created by Thompson on 10/31/17.
+//  Copyright © 2017 J.W.Enterprises LLC. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
